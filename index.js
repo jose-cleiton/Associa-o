085228dfe8745fbacce844917cc96f75096dc118
jose-cleiton <cleitons835@gmai.com>
2022-07-20 13:48:19 -0300

    Corrigindo Configuraçoes

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 // index.js
 const express = require('express');
-const { Address, Employee } = require('./models');
+
+require('express-async-errors');
 
 const app = express();
-
-app.get('/employees', async (_req, res) => {
-  try {
-    const employees = await Employee.findAll({
-      include: { model: Address, as: 'addresses' },
-    });
-
-    return res.status(200).json(employees);
-  } catch (e) {
-    console.log(e.message);
-    res.status(500).json({ message: 'Ocorreu um erro' });
-  };
-});
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
